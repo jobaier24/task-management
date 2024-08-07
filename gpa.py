cb222b0884d01327b2bcb016989dc05c646ffd4b
RJ Jobaier <coderjjobaier2017@gmail.com>
2024-08-07 11:11:50 +0600

    gpa calculatior & learning loop

---
+++ gpa.py
@@ -0,0 +1,10 @@
+subject_number = int(input("Subject number: "))
+
+marks = []
+
+for i in range(0,subject_number):
+    number = int(input("Enter your subject mark: "))
+    marks.append(number)
+    # print(number)
+
+
